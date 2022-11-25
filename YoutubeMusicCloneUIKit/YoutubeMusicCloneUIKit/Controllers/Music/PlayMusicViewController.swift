00b7a080121848d6dd5a1223221a0c14d36749a4
Barış Özgen <barisozgen@hotmail.com>
2022-11-25 11:12:50 +0300

    refactor: PlayMusicViewController animations

--- YoutubeMusicCloneUIKit/YoutubeMusicCloneUIKit/Controllers/Music/PlayMusicViewController.swift
+++ YoutubeMusicCloneUIKit/YoutubeMusicCloneUIKit/Controllers/Music/PlayMusicViewController.swift
@@ -50,7 +50,7 @@ class PlayMusicViewController: UIViewController, MusicSelectedDelegate {
         label.font = UIFont.systemFont(ofSize: 16, weight: .regular)
         label.numberOfLines = 2
         label.frame = CGRect(x: 92, y: 35, width: view.width - 120, height: 35)
-
+        
         return label
     }()
     
@@ -70,7 +70,7 @@ class PlayMusicViewController: UIViewController, MusicSelectedDelegate {
         button.setImage(UIImage(systemName: "play.fill"), for: .normal)
         button.frame = CGRect(x: self.view.right - 107, y: 20, width: 35, height: 35)
         button.addTarget(self, action: #selector(didTapPlay), for: .touchUpInside)
-
+        
         return button
     }()
     
@@ -89,15 +89,80 @@ class PlayMusicViewController: UIViewController, MusicSelectedDelegate {
         button.frame = CGRect(x: 29, y: 60, width: 35, height: 35)
         button.layer.opacity = 0
         button.addTarget(self, action: #selector(didTapPlayView), for: .touchUpInside)
-
+        
         return button
     }()
     
+    private lazy var likeButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.tintColor = .lightGray
+        button.setImage(UIImage(systemName: "hand.thumbsup"), for: .normal)
+        button.frame = CGRect(x: self.view.width - (29 + 35), y: 100 + self.view.width, width: 35, height: 35)
+        button.addTarget(self, action: #selector(didTapLike(_ :)), for: .touchUpInside)
+        button.layer.opacity = 0
+        return button
+    }()
+    
+    private lazy var unlikeButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.tintColor = .lightGray
+        button.setImage(UIImage(systemName: "hand.thumbsdown"), for: .normal)
+        button.frame = CGRect(x: 29, y: 100 + self.view.width, width: 35, height: 35)
+        button.addTarget(self, action: #selector(didTapLike(_ :)), for: .touchUpInside)
+        button.layer.opacity = 0
+        return button
+    }()
+    
+    private lazy var durationSlider: UISlider = {
+        let slider = UISlider()
+        slider.maximumValue = 0
+        slider.maximumValue = 0
+        slider.frame = CGRect(x: 29, y: 200 + view.width, width: view.width - 58, height: 35)
+        slider.layer.opacity = 0
+        return slider
+    }()
     //MARK: - LifeCycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
+    }
+    
+    //MARK: - Notification
+    @objc func showMusicDetailScreen(_ notification: Notification) {
+        if let viewModel = notification.object as? MusicViewModel {
+            view.layer.opacity = 1
+            viewModel.downloadImage {[weak self] image in
+                
+                self?.musicBackgroundImageView.image = image
+                self?.musicImageView.image = image
+                
+                self?.titleLabel.text = viewModel.title
+                self?.artistButton.text = viewModel.artist
+                
+                self?.durationSlider.maximumValue = Float(viewModel.music.durationInSeconds)
+                
+                self?.musicBackgroundImageView.setHeight(self?.expandedViewHeight ?? 100)
+                self?.musicBackgroundImageView.setWidth(self?.expandedViewHeight ?? 100)
+                
+                let blurEffect = UIBlurEffect(style: .light)
+                let blurEffectView = UIVisualEffectView(effect: blurEffect)
+                if let bounds = self?.view.bounds {
+                    blurEffectView.frame =  bounds
+                    blurEffectView.tag = 129
+                    self?.musicBackgroundImageView.addSubview(blurEffectView)
+                }
+                
+                
+                UIView.animate(withDuration: 0.7) {
+                    
+                    self?.musicBackgroundImageView.layer.opacity = 0.29
+                    
+                }
+            }
+            didTapPlayView()
+        }
+        
     }
     
     //MARK: - Helpers
@@ -127,40 +192,66 @@ class PlayMusicViewController: UIViewController, MusicSelectedDelegate {
         view.addSubview(playAndReflectButton)
         view.addSubview(nextAndOptionButton)
         view.addSubview(closeButton)
+        
+        view.addSubview(likeButton)
+        view.addSubview(unlikeButton)
+        
+        view.addSubview(durationSlider)
     }
     
-    //MARK: - Notification
-    @objc func showMusicDetailScreen(_ notification: Notification) {
-        if let viewModel = notification.object as? MusicViewModel {
-            view.layer.opacity = 1
-            viewModel.downloadImage {[weak self] image in
-                self?.musicBackgroundImageView.image = image
-                self?.musicImageView.image = image
-                
-                self?.titleLabel.text = viewModel.title
-                self?.artistButton.text = viewModel.artist
-                
-                self?.musicBackgroundImageView.setHeight(self?.expandedViewHeight ?? 100)
-                self?.musicBackgroundImageView.setWidth(self?.expandedViewHeight ?? 100)
-
-                let blurEffect = UIBlurEffect(style: .light)
-                let blurEffectView = UIVisualEffectView(effect: blurEffect)
-                if let bounds = self?.view.bounds {
-                    blurEffectView.frame =  bounds
-                    blurEffectView.tag = 129
-                    self?.musicBackgroundImageView.addSubview(blurEffectView)
-                }
-              
-                
-                UIView.animate(withDuration: 0.7) {
-                    
-                    self?.musicBackgroundImageView.layer.opacity = 0.29
-                    
-                }
-            }
-            didTapPlayView()
-        }
+    //MARK: - Animations
+    
+    func foldingAnimationQuick(){
+        closeButton.layer.opacity = 0
         
+        playAndReflectButton.setImage(UIImage(systemName: "play.fill"), for: .normal)
+        playAndReflectButton.frame = CGRect(x: view.right - 107, y: 20, width: 35, height: 35)
+        nextAndOptionButton.setImage(UIImage(systemName: "forward.end.fill"), for: .normal)
+        nextAndOptionButton.frame = CGRect(x: view.right - 55, y: 20, width: 35, height: 35)
+        musicBackgroundImageView.layer.opacity = 0
+        
+        titleLabel.frame = CGRect(x: 92, y: 10, width: view.width - 120, height: 35)
+        titleLabel.textAlignment = .left
+        titleLabel.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        
+        artistButton.frame = CGRect(x: 92, y: 35, width: view.width - 120, height: 35)
+        artistButton.textAlignment = .left
+        
+        likeButton.layer.opacity = 0
+        unlikeButton.layer.opacity = 0
+        durationSlider.layer.opacity = 0
+    }
+    func foldingAnimationSlow(){
+        
+        bottomBorder.backgroundColor = .gray.withAlphaComponent(0.29)
+        musicImageView.frame = CGRect(x: 29, y: 14, width: 48, height: 48)
+        
+    }
+    func expandingAnimationQuick(){
+        bottomBorder.backgroundColor = .gray.withAlphaComponent(0)
+        
+        playAndReflectButton.setImage(UIImage(systemName: "dot.radiowaves.up.forward"), for: .normal)
+        playAndReflectButton.frame = CGRect(x: view.right - 100, y: 60, width: 35, height: 35)
+        nextAndOptionButton.setImage(UIImage(systemName: "square.and.arrow.up"), for: .normal)
+        nextAndOptionButton.frame = CGRect(x: view.right - 60, y: 60, width: 35, height: 35)
+        
+        titleLabel.frame = CGRect(x: 70, y: 100 + view.width, width: view.width - 140, height: 40)
+        titleLabel.textAlignment = .center
+        titleLabel.font = UIFont.systemFont(ofSize: 30, weight: .heavy)
+        
+        artistButton.frame = CGRect(x: 60, y: 130 + view.width, width: view.width - 120, height: 40)
+        artistButton.textAlignment = .center
+    }
+    func expandingAnimationSlow(){
+        
+        musicImageView.frame = CGRect(x: 29, y: 129, width: view.width - 58, height: view.width - 58)
+        
+        musicBackgroundImageView.layer.opacity = 0.29
+        closeButton.layer.opacity = 1
+        
+        likeButton.layer.opacity = 1
+        unlikeButton.layer.opacity = 1
+        durationSlider.layer.opacity = 1
     }
     
     //MARK: - Actions
@@ -177,30 +268,14 @@ class PlayMusicViewController: UIViewController, MusicSelectedDelegate {
                 
                 bottomMusicPlayView.frame =  CGRect(x: 0, y: self.expandedViewHeight - (self.foldedViewHeight + 82), width: self.view.width, height: self.foldedViewHeight + 200 )
                 
-                self.bottomBorder.backgroundColor = .gray.withAlphaComponent(0)
-                
-                self.playAndReflectButton.setImage(UIImage(systemName: "dot.radiowaves.up.forward"), for: .normal)
-                self.playAndReflectButton.frame = CGRect(x: self.view.right - 100, y: 60, width: 35, height: 35)
-                self.nextAndOptionButton.setImage(UIImage(systemName: "square.and.arrow.up"), for: .normal)
-                self.nextAndOptionButton.frame = CGRect(x: self.view.right - 60, y: 60, width: 35, height: 35)
-                
-                self.titleLabel.frame = CGRect(x: 70, y: 100 + self.view.width, width: self.view.width - 140, height: 40)
-                self.titleLabel.textAlignment = .center
-                self.titleLabel.font = UIFont.systemFont(ofSize: 30, weight: .heavy)
-                
-                self.artistButton.frame = CGRect(x: 60, y: 130 + self.view.width, width: self.view.width - 120, height: 40)
-                self.artistButton.textAlignment = .center
+                self.expandingAnimationQuick()
                 
             }) {_ in
                 UIView.animate(withDuration: 0.7) {
                     
                     bottomMusicPlayView.frame = CGRect(x: 0, y: 0, width: self.view.width, height: self.expandedViewHeight)
                     
-                    self.musicImageView.frame = CGRect(x: 29, y: 129, width: self.view.width - 58, height: self.view.width - 58)
-                    
-                    self.musicBackgroundImageView.layer.opacity = 0.29
-                    self.closeButton.layer.opacity = 1
-
+                    self.expandingAnimationSlow()
                 }
             }
         }else {
@@ -208,28 +283,14 @@ class PlayMusicViewController: UIViewController, MusicSelectedDelegate {
                 
                 bottomMusicPlayView.frame =  CGRect(x: 0, y: 0, width: self.view.width, height: self.expandedViewHeight + 200)
                 
-                self.closeButton.layer.opacity = 0
-                
-                self.playAndReflectButton.setImage(UIImage(systemName: "play.fill"), for: .normal)
-                self.playAndReflectButton.frame = CGRect(x: self.view.right - 107, y: 20, width: 35, height: 35)
-                self.nextAndOptionButton.setImage(UIImage(systemName: "forward.end.fill"), for: .normal)
-                self.nextAndOptionButton.frame = CGRect(x: self.view.right - 55, y: 20, width: 35, height: 35)
-                self.musicBackgroundImageView.layer.opacity = 0
-                
-                self.titleLabel.frame = CGRect(x: 92, y: 10, width: self.view.width - 120, height: 35)
-                self.titleLabel.textAlignment = .left
-                self.titleLabel.font = UIFont.systemFont(ofSize: 18, weight: .bold)
-                
-                self.artistButton.frame = CGRect(x: 92, y: 35, width: self.view.width - 120, height: 35)
-                self.artistButton.textAlignment = .left
+                self.foldingAnimationQuick()
                 
             }) {_ in
                 UIView.animate(withDuration: 0.7) {
                     bottomMusicPlayView.frame =  CGRect(x: 0, y: self.expandedViewHeight - (self.foldedViewHeight + 82), width: self.view.width, height: self.foldedViewHeight)
                     
-                    self.bottomBorder.backgroundColor = .gray.withAlphaComponent(0.29)
-                    self.musicImageView.frame = CGRect(x: 29, y: 14, width: 48, height: 48)
-
+                    self.foldingAnimationSlow()
+                    
                     if let blurEffectView = self.view.subviews.first(where: { $0.tag == 129 }) {
                         blurEffectView.removeFromSuperview()
                     }
@@ -241,5 +302,25 @@ class PlayMusicViewController: UIViewController, MusicSelectedDelegate {
     
     @objc func didTapPlay(){
         playAndReflectButton.setImage(UIImage(systemName: "pause.fill"), for: .normal)
+    }
+    
+    @objc func didTapLike(_ sender: UIButton){
+        if sender == likeButton {
+            if likeButton.currentImage == UIImage(systemName: "hand.thumbsup.fill") {
+                likeButton.setImage(UIImage(systemName: "hand.thumbsup"), for: .normal)
+            }else {
+                likeButton.setImage(UIImage(systemName: "hand.thumbsup.fill"), for: .normal)
+            }
+            
+            unlikeButton.setImage(UIImage(systemName: "hand.thumbsdown"), for: .normal)
+        }else {
+            if unlikeButton.currentImage == UIImage(systemName: "hand.thumbsup.fill") {
+                unlikeButton.setImage(UIImage(systemName: "hand.thumbsdown"), for: .normal)
+            }else {
+                unlikeButton.setImage(UIImage(systemName: "hand.thumbsdown.fill"), for: .normal)
+            }
+            likeButton.setImage(UIImage(systemName: "hand.thumbsup"), for: .normal)
+            
+        }
     }
 }
